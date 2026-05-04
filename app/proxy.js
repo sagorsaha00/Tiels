b6f088206692ede1b84418878d82a490e8c832b7
Sagor saha <117963424+sagorsaha00@users.noreply.github.com>
2026-05-04 19:59:42 +0600

    add proxy file

---
+++ app/proxy.js
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import { auth } from "./utils/auth";
+
+export async function proxy(request) {
+    const session = await auth.api.getSession({
+        headers: request.headers,
+    });
+
+    if (!session) {
+        const loginUrl = new URL("/login", request.url);
+
+        return NextResponse.redirect(loginUrl);
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: ["/profile", "/update-profile", "/showProduct/:path*"],
+};
