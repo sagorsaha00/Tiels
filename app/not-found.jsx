d6140eacc0344c5abe997a46c31567d2233c7637
Sagor saha <117963424+sagorsaha00@users.noreply.github.com>
2026-05-04 22:54:30 +0600

    add header and footer in not found

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -2,9 +2,10 @@
 
 import Link from "next/link";
 import { useSpring, animated } from "@react-spring/web";
+import Header from "./components/header";
+import Footer from "./components/Footer";
 
 export default function NotFound() {
- 
   const fadeUp = useSpring({
     from: { opacity: 0, transform: "translateY(40px)" },
     to: { opacity: 1, transform: "translateY(0px)" },
@@ -18,34 +19,34 @@ export default function NotFound() {
   });
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-900-400 text-white px-6">
-      <div className="text-center">
-        
-        <animated.h1
-          style={scale}
-          className="text-7xl md:text-9xl font-extrabold bg-gradient-to-r from-purple-500 to-pink-500 text-transparent bg-clip-text"
-        >
-          404
-        </animated.h1>
-
-        <animated.div style={fadeUp}>
-          <h2 className="mt-4 text-2xl md:text-3xl font-semibold">
-            Page Not Found
-          </h2>
-
-          <p className="mt-2 text-gray-400">
-            Your page do not find 
-          </p>
-
-      
-          <Link
-            href="/"
-            className="inline-block mt-6 px-6 py-3 rounded-xl bg-gradient-to-r from-purple-500 to-pink-500 hover:scale-105 transition"
+    <>
+      <Header></Header>
+      <div className="min-h-screen flex items-center justify-center bg-gray-900-400 text-white px-6">
+        <div className="text-center">
+          <animated.h1
+            style={scale}
+            className="text-7xl md:text-9xl font-extrabold bg-gradient-to-r from-purple-500 to-pink-500 text-transparent bg-clip-text"
           >
-            Go Home
-          </Link>
-        </animated.div>
+            404
+          </animated.h1>
+
+          <animated.div style={fadeUp}>
+            <h2 className="mt-4 text-2xl md:text-3xl font-semibold">
+              Page Not Found
+            </h2>
+
+            <p className="mt-2 text-gray-400">Your page do not find</p>
+
+            <Link
+              href="/"
+              className="inline-block mt-6 px-6 py-3 rounded-xl bg-gradient-to-r from-purple-500 to-pink-500 hover:scale-105 transition"
+            >
+              Go Home
+            </Link>
+          </animated.div>
+        </div>
       </div>
-    </div>
+      <Footer></Footer>
+    </>
   );
 }
